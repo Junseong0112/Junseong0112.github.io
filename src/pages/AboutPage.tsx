d8fb712869aa76fc116aef0c52dd47389bf51e87
Jun Seong <drawbeat0112@gmail.com>
2024-08-13 19:57:04 +0900

    about page 골조완료

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -52,7 +52,53 @@ export default function AboutPage() {
           </div>
           <div>
             <h3 className="eng">Interests</h3>
-            <article></article>
+            <article>
+              <Icon name="exercise" />
+              <div>
+                <p>운동을 좋아해요</p>
+                <span>헬스, 러닝을 즐겨해요 운동은 스트레스 풀기에 제격</span>
+              </div>
+            </article>
+            <article>
+              <Icon name="communication" />
+              <div>
+                <p>대화를 좋아해요</p>
+                <span>
+                  다른 사람들과 얘기를 통해 지식을 담는 것을 좋아해요 새로운
+                  세상에 눈뜬 기분
+                </span>
+              </div>
+            </article>
+            <article>
+              <Icon name="food" />
+              <div>
+                <p>음식을 좋아해요</p>
+                <span>
+                  다양한 음식을 먹는것을 좋아해요 음식을 좋아해 요리를 즐겨해요
+                </span>
+              </div>
+            </article>
+          </div>
+          <div>
+            <h3 className="eng">Certificates</h3>
+            <div>
+              <p>
+                <span>
+                  GTQi(그래픽기술자격 일러스트) 1급 | 한국생산성본부 | 2022.01
+                </span>
+              </p>
+              <p>
+                <span>GTQ(그래픽기술자격) 1급 | 한국생산성본부 | 2021.10</span>
+              </p>
+              <p>
+                <span>
+                  정보기술자격(ITQ) 한글파워포인트 B | 한국생산성본부 | 2014.05
+                </span>
+              </p>
+              <p>
+                <span>워드프로세서 | 대한상공회의소 | 2013.03</span>
+              </p>
+            </div>
           </div>
         </article>
       </section>
